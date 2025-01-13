2ab337ff4215977e7177cb5b4eb6136a2a2197b7
Ruslan Khurramov <rxmedia77@gmail.com>
2025-01-13 07:21:32 +0500

    change on js

--- js/main.js
+++ js/main.js
@@ -4,23 +4,15 @@ modeswitch.addEventListener("pointerdown", () => {
     document.body.classList.toggle("daymode");
 })
 
-let hltext = document.querySelectorAll(".hl_txt");
-let icon = document.querySelectorAll(".ico");
+let hltext = document.querySelector(".hl_txt");
+let icon = document.querySelector(".ico");
 
-hltext.addEventListener("mouseover", () =>{
+hltext.addEventListener("mouseover", () => {
     icon.style.color = ("#3BB77E");
     hltext.style.color = ("#3BB77E");
-})
-icon.addEventListener("mouseover", () =>{
+});
+icon.addEventListener("mouseover", () => {
     icon.style.color = ("#3BB77E");
     hltext.style.color = ("#3BB77E");
-})
+});
 
-hltext.addEventListener("mouseout", () =>{
-    icon.style.color = ("");
-    hltext.style.color = ("");
-})
-icon.addEventListener("mouseout", () =>{
-    icon.style.color = ("");
-    hltext.style.color = ("");
-})
