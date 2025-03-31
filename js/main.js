1c5f43c5e8760537ae0f4676272d59a251a4bb39
Ruslan Khurramov <rxmedia77@gmail.com>
2025-03-31 11:43:43 +0500

    updated

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let modeswitch = document.querySelector("#modeswitcher");
 
 modeswitch.addEventListener("pointerdown", () => {
     document.body.classList.toggle("daymode");
-})
+});
 
 let hltext = document.querySelectorAll(".hl_txt");
 let icon = document.querySelectorAll(".ico");
